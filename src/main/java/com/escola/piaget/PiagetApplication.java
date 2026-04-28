2e666640d9149b4869046ba7d204b599279ddf64
xablau-xit <vh774025@gmail.com>
2026-04-28 20:35:01 -0300

    Add files via upload

---
+++ src/main/java/com/escola/piaget/PiagetApplication.java
@@ -0,0 +1,13 @@
+package com.escola.piaget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiagetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiagetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escola/piaget/PiagetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escola.piaget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiagetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
